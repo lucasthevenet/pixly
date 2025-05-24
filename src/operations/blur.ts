153c7e9e0df74e76d5c9f417c8f7d5553e3de367
lucasthevenet <lucastcervantes@gmail.com>
2025-05-24 00:49:02 -0300

    checkpoint

--- src/operations/blur.ts
+++ src/operations/blur.ts
@@ -10,10 +10,10 @@ const updateDataTextureFromChannelArrs = (
 	arrA: number[],
 ) => {
 	for (let i = 0; i < w * h; i++) {
-		rgba[i * 4] = arrR[i];
-		rgba[i * 4 + 1] = arrG[i];
-		rgba[i * 4 + 2] = arrB[i];
-		rgba[i * 4 + 3] = arrA[i];
+		rgba[i * 4] = arrR[i]!;
+		rgba[i * 4 + 1] = arrG[i]!;
+		rgba[i * 4 + 2] = arrB[i]!;
+		rgba[i * 4 + 3] = arrA[i]!;
 	}
 
 	return rgba;
@@ -24,16 +24,16 @@ const splitDataTextureIntoChannelArrays = (
 	w: number,
 	h: number,
 ): [number[], number[], number[], number[]] => {
-	const arrR = [];
-	const arrG = [];
-	const arrB = [];
-	const arrA = [];
+	const arrR: number[] = [];
+	const arrG: number[] = [];
+	const arrB: number[] = [];
+	const arrA: number[] = [];
 
 	for (let i = 0; i < w * h; i++) {
-		arrR.push(rgba[i * 4]);
-		arrG.push(rgba[i * 4 + 1]);
-		arrB.push(rgba[i * 4 + 2]);
-		arrA.push(rgba[i * 4 + 3]);
+		arrR.push(rgba[i * 4]!);
+		arrG.push(rgba[i * 4 + 1]!);
+		arrB.push(rgba[i * 4 + 2]!);
+		arrA.push(rgba[i * 4 + 3]!);
 	}
 
 	return [arrR, arrG, arrB, arrA];
@@ -47,9 +47,9 @@ const gaussBlur_4 = (
 	_r: number,
 	bxs: number[],
 ) => {
-	boxBlur_4(scl, tcl, w, h, (bxs[0] - 1) / 2);
-	boxBlur_4(tcl, scl, w, h, (bxs[1] - 1) / 2);
-	boxBlur_4(scl, tcl, w, h, (bxs[2] - 1) / 2);
+	boxBlur_4(scl, tcl, w, h, (bxs[0]! - 1) / 2);
+	boxBlur_4(tcl, scl, w, h, (bxs[1]! - 1) / 2);
+	boxBlur_4(scl, tcl, w, h, (bxs[2]! - 1) / 2);
 };
 
 const boxBlur_4 = (
@@ -60,7 +60,7 @@ const boxBlur_4 = (
 	r: number,
 ) => {
 	for (let i = 0; i < scl.length; i++) {
-		tcl[i] = scl[i];
+		tcl[i] = scl[i]!;
 	}
 
 	boxBlurH_4(tcl, scl, w, h, r);
@@ -81,26 +81,26 @@ const boxBlurH_4 = (
 		let li = ti;
 		let ri = ti + r;
 
-		const fv = scl[ti];
-		const lv = scl[ti + w - 1];
+		const fv = scl[ti]!;
+		const lv = scl[ti + w - 1]!;
 		let val = (r + 1) * fv;
 
 		for (let j = 0; j < r; j++) {
-			val += scl[ti + j];
+			val += scl[ti + j]!;
 		}
 
 		for (let j = 0; j <= r; j++) {
-			val += scl[ri++] - fv;
+			val += scl[ri++]! - fv;
 			tcl[ti++] = val * iarr;
 		}
 
 		for (let j = r + 1; j < w - r; j++) {
-			val += scl[ri++] - scl[li++];
+			val += scl[ri++]! - scl[li++]!;
 			tcl[ti++] = val * iarr;
 		}
 
 		for (let j = w - r; j < w; j++) {
-			val += lv - scl[li++];
+			val += lv - scl[li++]!;
 			tcl[ti++] = val * iarr;
 		}
 	}
@@ -119,23 +119,23 @@ const boxBlurT_4 = (
 		let ti = i;
 		let li = ti;
 		let ri = ti + r * w;
-		const fv = scl[ti];
-		const lv = scl[ti + w * (h - 1)];
+		const fv = scl[ti]!;
+		const lv = scl[ti + w * (h - 1)]!;
 		let val = (r + 1) * fv;
 
 		for (let j = 0; j < r; j++) {
-			val += scl[ti + j * w];
+			val += scl[ti + j * w]!;
 		}
 
 		for (let j = 0; j <= r; j++) {
-			val += scl[ri] - fv;
+			val += scl[ri]! - fv;
 			tcl[ti] = val * iarr;
 			ri += w;
 			ti += w;
 		}
 
 		for (let j = r + 1; j < h - r; j++) {
-			val += scl[ri] - scl[li];
+			val += scl[ri]! - scl[li]!;
 			tcl[ti] = val * iarr;
 			li += w;
 			ri += w;
@@ -143,7 +143,7 @@ const boxBlurT_4 = (
 		}
 
 		for (let j = h - r; j < h; j++) {
-			val += lv - scl[li];
+			val += lv - scl[li]!;
 			tcl[ti] = val * iarr;
 			li += w;
 			ti += w;
