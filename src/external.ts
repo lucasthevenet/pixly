075d2479e8d7d8d02fb039789958e7ca9f1cfeca
lucasthevenet <lucastcervantes@gmail.com>
2025-05-25 22:01:51 -0300

    checkpoint

--- src/external.ts
+++ src/external.ts
@@ -1,13 +1,9 @@
 export * from "./operations";
 export * from "./codecs";
 
-export type { ChannelOptions } from "./operations/adjust-channels";
-export type { GrayscaleOptions } from "./operations/grayscale";
-export type { TintOptions } from "./operations/tint";
+export { apply, decoder, encoder } from "./core";
 
-export { apply, decoder, encoder } from "./builder";
-
-export type { ImageEditor } from "./builder";
+export type { ImageEditor } from "./core";
 
 export type {
 	MimeType,
